a91b0884f2e609b0d15f2a7c61afca220f2b2228
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2011-11-04 14:49:30 +0530

    Main Layout Complete

--- src/com/vinsol/expensetracker/MainActivity.java
+++ src/com/vinsol/expensetracker/MainActivity.java
@@ -2,6 +2,7 @@ package com.vinsol.expensetracker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,27 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //temp view of graph
+        //******start view******//
+        float[] values = new float[] { 200.0f,100.5f, 22.5f, 140.0f , 3.0f,89.0f,72.0f };
+		String[] horlabels = new String[] { "mon", "tue", "wed", "thu","fri","sat","sun" };
+		GraphView graphView = new GraphView(this, values, "GraphTest",horlabels);
+		
+		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+						LinearLayout.LayoutParams.FILL_PARENT,
+						LinearLayout.LayoutParams.WRAP_CONTENT
+						);
+		
+		params.setMargins(0, 40, 0, 5);
+		
+		LinearLayout layout  = (LinearLayout) findViewById(R.id.main_graph);
+		layout.addView(graphView, params);
+		//******stop view******//
+		
+		
+		
+		
+		
     }
 }
