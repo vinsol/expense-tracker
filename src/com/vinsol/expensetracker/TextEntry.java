3e5d9021b23cd3fe3317aad2ba9a4565801dbf8c
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2011-11-08 19:18:15 +0530

    Nov 8 Final Commit

--- src/com/vinsol/expensetracker/TextEntry.java
+++ src/com/vinsol/expensetracker/TextEntry.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class TextEntry extends Activity {
 
@@ -17,8 +19,19 @@ public class TextEntry extends Activity {
         
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         
-        setContentView(R.layout.text_voice);
+        setContentView(R.layout.text_voice_camera);
         Log.v("Text", "Text");
+        
+        ///////  ******* Sets Header Margin  ******* ////////
+        LayoutParams params = new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.WRAP_CONTENT);
+        params.setMargins(0, 30, 0, 20);
+        
+        EditText text_voice_camera_amount = (EditText) findViewById(R.id.text_voice_camera_amount);
+        text_voice_camera_amount.setLayoutParams(params);
+        
+        
+        
+        
 	}
 	
 }
--- src/com/vinsol/expensetracker/Voice.java
+++ src/com/vinsol/expensetracker/Voice.java
@@ -3,7 +3,11 @@ package com.vinsol.expensetracker;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class Voice extends Activity {
 	
@@ -16,8 +20,26 @@ public class Voice extends Activity {
         
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         
-        setContentView(R.layout.text_voice);
+        setContentView(R.layout.text_voice_camera);
         Log.v("Voice", "Voice");
+        
+        ///////   ***** Sets Title Voice Entry *********///////
+        TextView text_voice_camera_header_title = (TextView) findViewById(R.id.text_voice_camera_header_title);
+        text_voice_camera_header_title.setText("Voice Entry");
+        
+        ///////   ***** Sets Title Voice Entry *********///////
+        ImageView text_voice_camera_voice_details_separator = (ImageView) findViewById(R.id.text_voice_camera_voice_details_separator);
+        text_voice_camera_voice_details_separator.setVisibility(View.VISIBLE);
+        
+        
+        
+        //////   ******  Shows Voice Details ********////////
+        
+        RelativeLayout text_voice_camera_voice_details = (RelativeLayout) findViewById(R.id.text_voice_camera_voice_details);
+        text_voice_camera_voice_details.setVisibility(View.VISIBLE);
+        
+        
+        
 	}
 	
 }
