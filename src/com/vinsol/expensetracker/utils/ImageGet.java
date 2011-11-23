4948c57f96358eaa35b6fa912ba8c6a859990c87
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2011-11-24 00:51:52 +0530

    Removed Crashes both for tablet and fones app almost stable report bugs if u find any

--- src/com/vinsol/expensetracker/utils/ImageGet.java
+++ src/com/vinsol/expensetracker/utils/ImageGet.java
@@ -1,10 +1,6 @@
 package com.vinsol.expensetracker.utils;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -13,21 +9,10 @@ import android.widget.Toast;
 public class ImageGet {
 
 	private String mFileName;
-	private File mFile;
-
-	private int width;
-	private int height;
-	
+	private File mFile;	
 	
 	///////   *********    ExpenseTracker Directory Location  ********  /////////
 	private File mExpenseTrackerDirectory; 
-	
-	///////   *******    Declaring Constants   ********   ///////////
-	private int SMALL_MAX_HEIGHT = 120;
-	private int SMALL_MAX_WIDTH = 160;
-
-	private int THUMBNAIL_MAX_HEIGHT = 60;
-	private int THUMBNAIL_MAX_WIDTH = 60;
 	
 	private Context mContext;
 	
@@ -37,26 +22,6 @@ public class ImageGet {
 		if(android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)){
 			mFileName = file;
 			mExpenseTrackerDirectory = new File("/sdcard/ExpenseTracker");
-		
-			File mPathImageByCamera = new File(mExpenseTrackerDirectory, file+".jpg");
-			FileInputStream fileInputStream = null;
-			try {
-				fileInputStream = new FileInputStream(mPathImageByCamera);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-			Bitmap imageByCamera = BitmapFactory.decodeStream(fileInputStream);
-			try {
-				fileInputStream.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		
-			if(imageByCamera.getHeight() > imageByCamera.getWidth()){
-				SMALL_MAX_WIDTH = 120;
-				SMALL_MAX_HEIGHT = 160;
-			}
-			imageByCamera.recycle();
 		} else {
 			Toast.makeText(mContext, "sdcard not available", Toast.LENGTH_LONG).show();
 		}
@@ -66,9 +31,8 @@ public class ImageGet {
 	public Bitmap getSmallImage(){
 		if(android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)){
 			mFile = new File(mExpenseTrackerDirectory,mFileName+"_small"+".jpg");
-			height = SMALL_MAX_HEIGHT;
-			width = SMALL_MAX_WIDTH;
-			return getBitmap();
+			Bitmap bm = BitmapFactory.decodeFile(mFile.getPath());
+			return bm;
 		} else {
 			Toast.makeText(mContext, "sdcard not available", Toast.LENGTH_LONG).show();
 			return null;
@@ -80,54 +44,12 @@ public class ImageGet {
 	public Bitmap getThumbnailImage(){
 		if(android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)){
 			mFile = new File(mExpenseTrackerDirectory,mFileName+"_thumbnail"+".jpg");
-			height = THUMBNAIL_MAX_HEIGHT;
-			width = THUMBNAIL_MAX_WIDTH;
-			return getBitmap();
+			Bitmap bm = BitmapFactory.decodeFile(mFile.getPath());
+			return bm;
 		} else {
 			Toast.makeText(mContext, "sdcard not available", Toast.LENGTH_LONG).show();
 			return null;
 		}
 	} 
-	
-	
-	///////////   **********   get cropped bitmap Image   **********   ///////////
-	private Bitmap getBitmap(){
-		FileInputStream fileInputStream = null;
-		try {
-			fileInputStream = new FileInputStream(mFile);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		Bitmap tempBitmap = BitmapFactory.decodeStream(fileInputStream);
-		
-		int defaultHeight = tempBitmap.getHeight();
-        int defaultWidth = tempBitmap.getWidth();
-        int tempHeight = height;
-        int tempWidth = width;
-        int i = 1;
-        do{
-     	   tempHeight = height * i;
-     	   tempWidth = width * i;
-     	   i++;
-        }while(tempHeight < (defaultHeight-height) && tempWidth < (defaultWidth-width) );
-        i--;
-        
-        int diffHeight = defaultHeight - height;
-        int diffWidth = defaultWidth - width;
-        int x = diffWidth / 2;
-        int y = diffHeight / 2;
-        int finalx;
-        int finaly;
-        if(diffWidth % 2 == 0)
-        	   finalx = defaultWidth - x;
-        else
-     	   finalx = defaultWidth - x+1;
-        if(diffHeight % 2 == 0)
-        	   finaly = defaultHeight - y;
-        else
-     	   finaly = defaultHeight - y+1;
-		
-        return Bitmap.createBitmap(tempBitmap, x, y, finalx, finaly);
-	}
 	
 }
