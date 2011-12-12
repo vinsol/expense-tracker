bb17605fa23ed32e43e4049cc855c3f3735fa084
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2011-12-12 15:22:30 +0530

    Added Header and location to unknown entry layout

--- src/com/vinsol/expensetracker/UnknownEntryDialog.java
+++ src/com/vinsol/expensetracker/UnknownEntryDialog.java
@@ -5,23 +5,52 @@ import android.content.Context;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class UnknownEntryDialog extends Dialog implements android.view.View.OnClickListener {
 
 	private Button deleteButton;
+	private Button textEntryButton;
+	private Button voiceEntryButton;
+	private Button cameraEntryButton;
+	private Button favoriteEntryButton;
 	
 	public UnknownEntryDialog(Context mContext) {
 		super(mContext);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.unknown_entry_dialog);
+		textEntryButton = (Button) findViewById(R.id.main_text);
+		deleteButton = (Button) findViewById(R.id.unknown_entry_dialog_delete);
+		voiceEntryButton = (Button) findViewById(R.id.main_voice);
+		cameraEntryButton = (Button) findViewById(R.id.main_camera);
+		favoriteEntryButton = (Button) findViewById(R.id.main_favorite);
+		textEntryButton.setOnClickListener(this);
+		deleteButton.setOnClickListener(this);
+		voiceEntryButton.setOnClickListener(this);
+		cameraEntryButton.setOnClickListener(this);
+		favoriteEntryButton.setOnClickListener(this);
 		show();
 	}
 
 	@Override
 	public void onClick(View v) {
-		if (v.getId() == R.id.description_dialog_cancel_button) {
-			dismiss();
+		switch (v.getId()) {
+		case R.id.main_text:
+			
+			break;
+		case R.id.unknown_entry_dialog_delete:
+			
+			break;
+		case R.id.main_voice:
+				
+			break;
+		case R.id.main_camera:
+			
+			break;
+		case R.id.main_favorite:
+	
+			break;
+		default:
+			break;
 		}
 	}
 }
