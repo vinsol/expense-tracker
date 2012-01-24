f3799ff266b033b25a577aa8282553b30013f1f1
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2012-01-24 16:07:18 +0530

    Added Delete dialog implementation

--- src/com/vinsol/expensetracker/helpers/DeleteDialog.java
+++ src/com/vinsol/expensetracker/helpers/DeleteDialog.java
@@ -3,10 +3,14 @@ package com.vinsol.expensetracker.helpers;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.vinsol.expensetracker.R;
 
-public class DeleteDialog extends Dialog {
+public class DeleteDialog extends Dialog implements android.view.View.OnClickListener {
+	
+	private boolean isDelete;
 	
 	public DeleteDialog(Context context) { 
 		super(context);
@@ -16,6 +20,30 @@ public class DeleteDialog extends Dialog {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.delete_dialog);
+		Button yesButton = (Button) findViewById(R.id.delete_dialog_yes);
+		Button noButton = (Button) findViewById(R.id.delete_dialog_no);
+		yesButton.setOnClickListener(this);
+		noButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		dismiss();
+		switch (v.getId()) {
+		case R.id.delete_dialog_yes:
+			isDelete = true;
+			break;
+
+		case R.id.delete_dialog_no:
+			isDelete = false;
+			break;
+		default:
+			break;
+		}
+	}
+
+	public boolean isDelete() {
+		return isDelete;
 	}
 	
 }
