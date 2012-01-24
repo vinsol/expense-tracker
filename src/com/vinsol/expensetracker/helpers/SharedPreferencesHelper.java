6294767f1d5f4e0db94f7c2b91a6260e5640dc6a
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2012-01-24 16:39:53 +0530

    Added SharedPreferencesHelper which includes everything related with preferences of app

---
+++ src/com/vinsol/expensetracker/helpers/SharedPreferencesHelper.java
@@ -0,0 +1,29 @@
+package com.vinsol.expensetracker.helpers;
+
+import com.vinsol.expensetracker.R;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
+
+public class SharedPreferencesHelper {
+
+	private Context context;
+	
+	public SharedPreferencesHelper(Context context) {
+		this.context = context;
+	}
+	
+	public SharedPreferences getSharedPreferences() {
+		return PreferenceManager.getDefaultSharedPreferences(context);
+	}
+	
+	public void setDeletePrefs(Boolean isChecked) {
+		SharedPreferences sharedPreferences = getSharedPreferences();
+		Editor prefEditor = sharedPreferences.edit();
+		prefEditor.putBoolean(context.getString(R.string.pref_key_delete_dialog), isChecked);
+		prefEditor.commit();	
+	}
+	
+}
