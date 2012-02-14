b9bae18637ea89ce49a062b7b8c604dd4b0c9107
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2012-02-14 12:08:48 +0530

    Added xml to layouts and src files

--- src/com/vinsol/expensetracker/helpers/DeleteDialog.java
+++ src/com/vinsol/expensetracker/helpers/DeleteDialog.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2012 Vinayak Solutions Private Limited 
+ * See the file license.txt for copying permission.
+*/     
+
 package com.vinsol.expensetracker.helpers;
 
 import android.app.Dialog;
--- src/com/vinsol/expensetracker/utils/ImageViewExtended.java
+++ src/com/vinsol/expensetracker/utils/ImageViewExtended.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2012 Vinayak Solutions Private Limited 
+ * See the file license.txt for copying permission.
+*/     
+
 package com.vinsol.expensetracker.utils;
 
 import android.content.Context;
