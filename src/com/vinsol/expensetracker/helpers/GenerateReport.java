cf15b72f3003096666c3d8eccb1fe0ae93e6854c
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2012-03-12 13:35:22 +0530

    Generate Report class created

---
+++ src/com/vinsol/expensetracker/helpers/GenerateReport.java
@@ -0,0 +1,16 @@
+package com.vinsol.expensetracker.helpers;
+
+import android.os.Bundle;
+
+import com.vinsol.expensetracker.BaseActivity;
+import com.vinsol.expensetracker.R;
+
+public class GenerateReport extends BaseActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.generate_report);
+	}
+
+}
