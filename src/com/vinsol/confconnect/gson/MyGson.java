180c2088adffc8362eb12f0fc25f7f63ae971291
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2012-06-13 19:22:17 +0530

    version change

--- src/com/vinsol/confconnect/gson/MyGson.java
+++ src/com/vinsol/confconnect/gson/MyGson.java
@@ -6,11 +6,10 @@ import com.google.gson.GsonBuilder;
 public class MyGson {
 
 	public Gson get() {
-//		GsonBuilder gsonBuilder = new GsonBuilder();
-//		gsonBuilder.setFieldNamingStrategy(new ExpenseTrackerFieldNamingPolicy());
-////		gsonBuilder.setExclusionStrategies(new ExpenseTrackerExclusionStrategy());
-//		return gsonBuilder.create(); 
-		return new GsonBuilder().setFieldNamingStrategy(new ExpenseTrackerFieldNamingPolicy()).create();
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.setFieldNamingStrategy(new ExpenseTrackerFieldNamingPolicy());
+		gsonBuilder.setExclusionStrategies(new ExpenseTrackerExclusionStrategy());
+		return gsonBuilder.create(); 
 	}
 	
 }
