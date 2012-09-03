6d5e2592d55539997d19ca08c0e3e9a562eab648
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2012-09-03 16:21:19 +0530

    added documentation to CameraHardwareException class

--- src/com/vinsol/expensetracker/cameraservice/CameraHardwareException.java
+++ src/com/vinsol/expensetracker/cameraservice/CameraHardwareException.java
@@ -5,6 +5,10 @@
 
 package com.vinsol.expensetracker.cameraservice;
 
+/**
+ * This class represents the condition that we cannot open the camera hardware
+ * successfully. For example, another process is using the camera.
+ */
 public class CameraHardwareException extends Exception {
 
 	private static final long serialVersionUID = 8230180283056198888L;
